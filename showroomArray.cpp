e368e6c9a8c5b502b8accd0d3094f2566cb6104a
AdityaJarhad <adityajarhad9@gmail.com>
2024-04-25 21:32:37 +0530

    Hello

---
+++ showroomArray.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class vehicle
+{
+private:
+	int id;
+	string name;
+	float price;
+public:
+	showroom()
+	{
+		id=00;
+		name="NA";
+		price=00.00;	
+	}
+	showroom(int _id,string _name, float _price)
+	{
+		id=_id;
+		name=_name;
+		price=_price;
+	}
+	
+};
+
+void add()
+{
+	
+}
+
+void display()
+{
+	
+}
+
+void edit()
+{
+	
+}
+
+void move()
+{
+	
+}
+
+void price()
+{
+	
+}
+
+void search()
+{
+	
+}
+
+
+
+main()
+{
+	int size=100;
+	vehicle veh[size];
+	int count;
+	int choice;
+	do
+	{
+		cout<<"Enter Choice"<<endl;
+		switch(choice)
+		{
+				case 1:
+					
+					break;
+				case 2:
+					
+					break;
+				case 3:
+					
+					break;
+				case 4:
+					
+					break;
+				case 5:
+					
+					break;
+		}
+		
+		
+		
+		
+	}while(choice!=0);
+	
+	
+}
